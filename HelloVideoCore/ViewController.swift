6ae4fece001d6e616708f3b0c5fb83d298d6ae10
François Rouault <francois.rouault.fr@gmail.com>
2017-08-29 02:11:09 +0200

    Add image

--- HelloVideoCore/ViewController.swift
+++ HelloVideoCore/ViewController.swift
@@ -10,20 +10,39 @@ import Foundation
 import UIKit
 import videocore
 
+extension UIImage {
+    
+    func scaleImage(toSize newSize: CGSize) -> UIImage? {
+        let newRect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height).integral
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 0)
+        if let context = UIGraphicsGetCurrentContext() {
+            context.interpolationQuality = .high
+            let flipVertical = CGAffineTransform(a: 1, b: 0, c: 0, d: -1, tx: 0, ty: newSize.height)
+            context.concatenate(flipVertical)
+            context.draw(self.cgImage!, in: newRect)
+            let newImage = UIImage(cgImage: context.makeImage()!)
+            UIGraphicsEndImageContext()
+            return newImage
+        }
+        return nil
+    }
+    
+}
+
 class ViewController: UIViewController, VCSessionDelegate {
 
     @IBOutlet weak var cameraContainer: UIView!
     
-    var session:VCSimpleSession = VCSimpleSession(videoSize: CGSize(width: 1280, height: 720), frameRate: 30, bitrate: 1000000, useInterfaceOrientation: false)
-    
+    var session:VCSimpleSession!
+    let width = 1280
+    let height = 720
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        session  = VCSimpleSession(videoSize: CGSize(width: width, height: height), frameRate: 30, bitrate: 1000000, useInterfaceOrientation: false)
         cameraContainer.addSubview(session.previewView)
         session.previewView.frame = cameraContainer.bounds
         session.delegate = self
-        
-        
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,8 +50,21 @@ class ViewController: UIViewController, VCSessionDelegate {
         // Dispose of any resources that can be recreated.
     }
 
+    public func addImage(_ image: UIImage, inView view: UIView, origin: CGPoint, size: CGSize) {
+        //let containerSize = view.bounds.size
+        guard let img = image.scaleImage(toSize: size) else {
+            return
+        }
+        let r = CGRect(x: size.width/2 + origin.x, y: size.height/2 + origin.y, width: size.width, height: size.height)
+        session.addPixelBufferSource(img, with: r)
+    }
+    
     public func didAddCameraSource(_ session: VCSimpleSession!) {
-        session.filter = VCFilter.sepia
+//        session.filter = VCFilter.sepia
+        let vcViewSize = CGSize(width: width, height: height)//view.bounds.size
+        let gradientHeight = 0.3 * vcViewSize.height
+        let origin = CGPoint(x: 0, y: vcViewSize.height - gradientHeight)
+        addImage(UIImage(named: "logo")!, inView: view, origin: origin, size: CGSize(width: vcViewSize.width, height: gradientHeight))
     }
     
     func connectionStatusChanged(_ sessionState: VCSessionState) {
